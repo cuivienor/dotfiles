a66b6307405c61b29db6fb589f9495655baa3c8d
Peter Petrov <peter@petrovs.io>
2024-12-06 15:13:56 -0500

    Add initial  script

--- config/.config/path.bash
+++ config/.config/path.bash
@@ -1,5 +1,5 @@
 # Universal path locations
-export PATH=$HOME/.local/bin:$HOME/bin:/usr/local/bin:$PATH
+export PATH=$HOME/.local/scripts/:$HOME/.local/bin:$HOME/bin:/usr/local/bin:$PATH
 
 # Ghostty installation
 # TODO: Generalize this to a non arch specific convention
