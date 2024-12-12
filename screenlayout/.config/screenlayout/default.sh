1fedf1f1907f1d6a01fb167ad5606c9f5069aa1d
Peter Petrov <peter@petrovs.io>
2024-12-12 13:11:17 -0500

    Setup proper screen layout

---
+++ screenlayout/.config/screenlayout/default.sh
@@ -0,0 +1,8 @@
+xrandr --output DVI-D-0 --off \
+	--output HDMI-0 --off \
+	--output DP-0 --mode 3440x1440 --pos 0x0 --rotate left \
+	--output DP-1 --off \
+	--output DP-2 --mode 3840x2160 --scale 0.7x0.7 --pos 4000x752 --rotate right \
+	--output DP-3 --off \
+	--output DP-4 --primary --mode 2560x1440 --pos 1440x905 --rotate normal \
+	--output DP-5 --off
