0b7fe74ce74b610ef287a12ad02a3cc9be239b20
Peter Petrov <peter@petrovs.io>
2024-12-16 09:55:20 -0500

    Disable icon picker for now

--- nvim/.config/nvim/lua/plugins/icon-picker.lua
+++ nvim/.config/nvim/lua/plugins/icon-picker.lua
@@ -9,4 +9,5 @@ return {
 		vim.keymap.set("n", "<Leader><Leader>y", "<cmd>IconPickerYank<cr>", opts) --> Yank the selected icon into register
 		vim.keymap.set("i", "<C-i>", "<cmd>IconPickerInsert<cr>", opts)
 	end,
+	enabled = false,
 }
