03bc5f351b8ab1fe91ee3e0542bcf7b16d78d210
Peter Petrov <peter@petrovs.io>
2024-12-29 22:12:16 -0500

    Try cspell configuration

--- stow/nvim/.config/nvim/lua/plugins/none-ls.lua
+++ stow/nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -4,10 +4,14 @@ return {
 	config = function()
 		local null_ls = require("null-ls")
 		local cspell = require("cspell")
+		local config = {
+			config_file_preferred_name = "cspell.json",
+			cspell_config_dirs = { "~/.config/cspell/" },
+		}
 		null_ls.setup({
 			sources = {
-				cspell.diagnostics,
-				cspell.code_actions,
+				cspell.diagnostics.with({ config = config }),
+				cspell.code_actions.with({ config = config }),
 			},
 		})
 	end,
