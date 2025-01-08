8b2ba31f8e4ccff143ff78b85158f9f066c7eac7
Peter Petrov <peter@petrovs.io>
2025-01-08 15:06:48 -0500

    Change xrandr for new gpu

--- stow/screenlayout/.config/screenlayout/default.sh
+++ stow/screenlayout/.config/screenlayout/default.sh
@@ -1,8 +1,5 @@
-xrandr --output DVI-D-0 --off \
-	--output HDMI-0 --off \
-	--output DP-0 --mode 3440x1440 --pos 0x0 --rotate left \
-	--output DP-1 --off \
-	--output DP-2 --mode 3840x2160 --scale 0.7x0.7 --pos 4000x752 --rotate right \
-	--output DP-3 --off \
-	--output DP-4 --primary --mode 2560x1440 --pos 1440x905 --rotate normal \
-	--output DP-5 --off
+#!/bin/sh
+xrandr --output HDMI-A-1 --off \
+	--output DisplayPort-1 --mode 3440x1440 --pos 0x0 --rotate left \
+	--output HDMI-A-0 --mode 3840x2160 --pos 4000x752 --rotate right \
+	--output DisplayPort-0 --primary --mode 2560x1440 --pos 1440x905 --rotate normal
