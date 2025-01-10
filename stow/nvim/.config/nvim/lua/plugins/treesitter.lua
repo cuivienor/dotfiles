fa3d9a432a8ad7865d6523b87e7d6aa6dee909c8
Peter Petrov <peter@petrovs.io>
2025-01-10 15:01:36 -0500

    Add incremental selection for treesitter

--- stow/nvim/.config/nvim/lua/plugins/treesitter.lua
+++ stow/nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -19,10 +19,15 @@ return { -- Highlight, edit, and navigate code
 		auto_install = true,
 		highlight = {
 			enable = true,
-			-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-			--  If you are experiencing weird indenting issues, add the language to
-			--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-			additional_vim_regex_highlighting = { "ruby" },
+		},
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "gnn",
+				node_incremental = "gnn",
+				scope_incremental = "grc",
+				node_decremental = "gnm",
+			},
 		},
 		textobjects = {
 			select = {
@@ -69,9 +74,6 @@ return { -- Highlight, edit, and navigate code
 		indent = { enable = true, disable = { "ruby" } },
 	},
 	config = function(_, opts)
-		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-
-		-- Prefer git instead of curl in order to improve connectivity in some environments
 		require("nvim-treesitter.install").prefer_git = true
 
 		-- There is no treesitter zsh support so use bash instead
@@ -79,12 +81,5 @@ return { -- Highlight, edit, and navigate code
 
 		---@diagnostic disable-next-line: missing-fields
 		require("nvim-treesitter.configs").setup(opts)
-
-		-- There are additional nvim-treesitter modules that you can use to interact
-		-- with nvim-treesitter. You should go explore a few and see what interests you:
-		--
-		--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-		--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-		--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 	end,
 }
