c613b007628b090c80396b8dad546350e022752e
Peter Petrov <peter@petrovs.io>
2025-03-23 16:14:23 -0400

    Remove custom ghostty build path

--- stow/config/.config/path.bash
+++ stow/config/.config/path.bash
@@ -1,6 +1,2 @@
 # Universal path locations
 export PATH=$HOME/.local/scripts/:$HOME/.local/bin:$HOME/bin:/usr/local/bin:$PATH
-
-# Ghostty installation
-# TODO: Generalize this to a non arch specific convention
-export PATH=$HOME/src/ghostty-git/pkg/ghostty-git/usr/bin:$PATH
