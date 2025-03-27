0f52d0bd5fbf32705aa0fec4572b9069886b7905
Peter Petrov <peter@petrovs.io>
2025-03-27 09:11:19 -0400

    Make telescope live grep respect hidden files in git

--- stow/nvim/.config/nvim/lua/plugins/telescope.lua
+++ stow/nvim/.config/nvim/lua/plugins/telescope.lua
@@ -43,6 +43,18 @@ return { -- Fuzzy Finder (files, lsp, etc)
 			})
 		end
 
+		local grep_files_with_hidden = function()
+			builtin.live_grep({
+				additional_args = function(opts)
+					return {
+						"--hidden", -- Include hidden files
+						"--glob",
+						"!**/.git/*", -- Exclude .git directory
+					}
+				end,
+			})
+		end
+
 		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
 		vim.keymap.set("n", "<leader>sf", search_files, { desc = "[S]earch [F]iles" })
@@ -52,7 +64,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
 		vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "[S]earch [P]roject Files" })
 		vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
 		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+		vim.keymap.set("n", "<leader>sg", grep_files_with_hidden, { desc = "[S]earch by [G]rep" })
 		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
 		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
