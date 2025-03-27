6c20cf0ecb2bc5f3a9540d20cca2e265ccf773dd
Peter Petrov <peter@petrovs.io>
2025-03-27 09:36:23 -0400

    Disable markdown linter in gpt files

--- stow/nvim/.config/nvim/lua/plugins/lint.lua
+++ stow/nvim/.config/nvim/lua/plugins/lint.lua
@@ -3,6 +3,15 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
 		local lint = require("lint")
+
+		-- TODO: Move to a utilities place
+		local function should_disable_markdown_lint()
+			local excluded_base_path = vim.fn.expand("~/.local/share/nvim/gp/chats/")
+			local current_file_path = vim.fn.expand("%:p") -- Get full path to current file
+
+			return vim.startswith(current_file_path, excluded_base_path)
+		end
+
 		lint.linters_by_ft = {
 			markdown = { "markdownlint" },
 			sh = { "shellcheck" },
@@ -23,6 +32,9 @@ return {
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 			group = lint_augroup,
 			callback = function()
+				if should_disable_markdown_lint() then
+					lint.linters_by_ft.markdown = {}
+				end
 				require("lint").try_lint()
 			end,
 		})
